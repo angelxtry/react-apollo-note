06963bb413ec17b4a708a1fa114691d1315a6888
LeeSuHo <angelxtry@gmail.com>
2020-03-19 00:16:37 +0900

    Note Component 추가
    react-markdown package 설치.

--- src/Routes/Note/Note.tsx
+++ src/Routes/Note/Note.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { useParams, Link, RouteComponentProps } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
+import { GET_NOTE } from '../../queries';
 
-interface Props {}
+const TitleComponent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  margin: 0;
+  padding: 0;
+`;
+
+const Button = styled.button``;
+
+type Props = RouteComponentProps;
 
 const Note: React.FC<Props> = () => {
-  return <div>Hello</div>;
+  const params = useParams() as { id: string };
+  const { data } = useQuery(GET_NOTE, {
+    variables: { id: params.id },
+  });
+  console.log({ data, params });
+  return data && data.note ? (
+    <>
+      <TitleComponent>
+        <Title>{data.note && data.note.title}</Title>
+        <Link to={`/edit/${data.note.id}`}>
+          <Button>Edit</Button>
+        </Link>
+      </TitleComponent>
+      <ReactMarkdown source={'# This is a header\n\nAnd this is a paragraph'} />
+    </>
+  ) : (
+    <ReactMarkdown source={'# This is a header\n\nAnd this is a paragraph'} />
+  );
 };
 
 export default Note;
--- src/queries.ts
+++ src/queries.ts
@@ -1,11 +1,21 @@
 import gql from 'graphql-tag';
+import { NOTE_FRAGMENT } from './fragments';
 
 export const GET_NOTES = gql`
   {
     notes @client {
-      id
-      title
-      content
+      ...NotePart
     }
   }
+  ${NOTE_FRAGMENT}
 `;
+
+export const GET_NOTE = gql`
+  query getNote($id: Int!) {
+    note(id: $id) @client {
+      ...NotePart
+    }
+  }
+  ${NOTE_FRAGMENT}
+`;
+
