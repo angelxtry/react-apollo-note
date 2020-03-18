a1101262a7e409d2c8a58d7a74a3bac71bb0734f
LeeSuHo <angelxtry@gmail.com>
2020-03-18 16:58:19 +0900

    Add graphql type definitions
    graphql의 타입을 정의하고 default data를 설정.
    간단한 query를 app에서 실행하여 cache에 데이터 저장.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_NOTES } from './queries';
 
 function App() {
-  return <div className="App">App</div>;
+  const { loading, data } = useQuery(GET_NOTES);
+  return (
+    <div>
+      {!loading && data && data.notes && (
+        <>
+          <div>{data.notes[0].id}</div>
+          <div>{data.notes[0].title}</div>
+          <div>{data.notes[0].content}</div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/clientState.ts
+++ src/clientState.ts
@@ -1,15 +1,31 @@
 import gql from 'graphql-tag';
 
-export const defaults = {};
+export const defaults = {
+  notes: [
+    {
+      __typename: 'Note',
+      id: 1,
+      title: 'First',
+      content: 'Second',
+    },
+  ],
+};
 
 export const typeDefs = gql`
+  type Note {
+    id: Int!
+    title: String!
+    content: String!
+  }
   type Query {
-    ping: String!
+    notes: [Note]!
+    note(id: String!): Note
+  }
+  type Mutation {
+    createNote(title: String!, content: String!): Note
+    editNote(id: Int!, title: String!, content: String!): Note
   }
 `;
 
 export const resolvers = {
-  Query: {
-    pong: () => 'ping',
-  },
 };
