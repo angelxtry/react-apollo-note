5d2db338033735e30d016ff0ba661cc7f34ebdba
LeeSuHo <angelxtry@gmail.com>
2020-03-19 00:10:55 +0900

    global style 수정
    styled-reset 제거

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,24 +1,59 @@
-
-import reset from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
   /* other styles */
-  * {
-    box-sizing: border-box;
+  :root {
+    --greyColor: #A2A19E;
+    --blackColor: #373630;
   }
-  body {
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+  body{
+      background-color:#F7F5F3;
+      font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      color:var(--blackColor);
+      padding:50px 100px;
+      margin:0;
+  }
+  #root{
+      max-width:1000px;
+      width:100%;
+      margin:0 auto;
   }
   a {
-    color: inherit;
-    text-decoration: none;
+      color:inherit;
+      text-decoration:none;
+  }
+  div{
+      margin:0;
   }
   input,
-  button {
-    &:focus,
-    &:active{outline: none}
+  textarea{
+      appearance:none;
+      border:none;
+      background-color:transparent;
+      resize:none;
+      &::placeholder {
+          color: #E7E7E6;
+      }
+      &:focus,
+      &:active{
+          outline:none;
+      }
+  }
+  .markdown a{
+      text-decoration:underline;
+  }
+  button{
+      appearance:none;
+      border:none;
+      background-color:transparent;
+      font-weight:600;
+      font-size:15px;
+      cursor:pointer;
+      border:2.5px solid var(--blackColor);
+      &:focus,
+      &:active{
+          outline:none
+      }
   }
 `;
 
