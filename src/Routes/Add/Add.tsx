fa121e5dce446bc70cf0d89b71f0fc569b142407
LeeSuHo <angelxtry@gmail.com>
2020-03-19 15:52:12 +0900

    Add Add component

--- src/Routes/Add/Add.tsx
+++ src/Routes/Add/Add.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { useHistory, RouteComponentProps } from 'react-router-dom';
+import Editor from '../../Components/Editor';
+import { CREATE_NOTE } from '../../queries';
+import { Note, MutationCreateNoteArgs } from '../../types/graph';
 
-interface Props {}
+type Props = RouteComponentProps;
 
 const Add: React.FC<Props> = () => {
-  return <div>Hello</div>;
+  const history = useHistory();
+  const [createNoteMutation] = useMutation<Note, MutationCreateNoteArgs>(
+    CREATE_NOTE,
+  );
+  const mutationFn = async (title: string, content: string) => {
+    if (title !== '' && content !== '') {
+      await createNoteMutation({ variables: { title, content } });
+      history.push('/');
+    }
+  };
+
+  return <Editor onSave={mutationFn} />;
 };
 
 export default Add;
