6ae2c92176cd2acea0caf82fc152f66bc8ffa38c
LeeSuHo <angelxtry@gmail.com>
2020-03-19 22:37:29 +0900

    Type 추가 & try catch 에러 처리 추가

--- src/clientState.ts
+++ src/clientState.ts
@@ -1,13 +1,16 @@
 import gql from 'graphql-tag';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { Resolvers } from 'apollo-client';
 import { NOTE_FRAGMENT } from './fragments';
 import { GET_NOTES } from './queries';
 import { saveNoteAtLocalStorage, loadNoteFromLocalStorage } from './offline';
+import { Note } from './types/graph';
+import { Notes } from './types/types';
 
 const TYPE_NAME = 'Note' as const;
 
 export const defaults = {
-  notes: [...loadNoteFromLocalStorage()],
+  notes: loadNoteFromLocalStorage(),
 };
 
 export const typeDefs = gql`
@@ -26,61 +29,86 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {
+export const resolvers: Resolvers = {
   Query: {
     note: (
       _: undefined,
       { id }: { id: string },
       { cache }: { cache: InMemoryCache },
     ) => {
-      const noteId = `${TYPE_NAME}:${id}`;
-      const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id: noteId });
-      return note;
+      try {
+        const noteId = `${TYPE_NAME}:${id}`;
+        const note = cache.readFragment<Note, string>({
+          fragment: NOTE_FRAGMENT,
+          id: noteId,
+        });
+        return note;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return null;
+      }
     },
   },
   Mutation: {
-    createNote: (_: undefined, args: any, { cache }: any) => {
-      const data = cache.readQuery({ query: GET_NOTES }) as any;
-      const notes = [];
-      if (data) {
-        notes.push(...data.notes);
+    createNote: (
+      _: undefined,
+      args: Note,
+      { cache }: { cache: InMemoryCache },
+    ) => {
+      try {
+        const data = cache.readQuery<Notes>({ query: GET_NOTES });
+        const notes: Note[] = [];
+        if (data) {
+          notes.push(...data.notes);
+        }
+        const newNote = {
+          __typename: TYPE_NAME,
+          title: args.title,
+          content: args.content,
+          id: notes.length + 1,
+        };
+        cache.writeData({
+          data: {
+            notes: [newNote, ...notes],
+          },
+        });
+        saveNoteAtLocalStorage(cache);
+        return newNote;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return null;
       }
-      const newNote = {
-        __typename: TYPE_NAME,
-        title: args.title,
-        content: args.content,
-        id: notes.length + 1,
-      };
-      cache.writeData({
-        data: {
-          notes: [newNote, ...notes],
-        },
-      });
-      saveNoteAtLocalStorage(cache);
-      return newNote;
     },
     editNote: (
       _: undefined,
-      { id, title, content }: { id: number; title: string; content: string },
+      { id, title, content }: Note,
       { cache }: { cache: InMemoryCache },
     ) => {
-      const noteId = `${TYPE_NAME}:${id}`;
-      const note = cache.readFragment({
-        fragment: NOTE_FRAGMENT,
-        id: noteId,
-      }) as any[];
-      const updateNote = {
-        ...note,
-        title,
-        content,
-      };
-      cache.writeFragment({
-        id: noteId,
-        fragment: NOTE_FRAGMENT,
-        data: updateNote,
-      });
-      saveNoteAtLocalStorage(cache);
-      return updateNote;
+      try {
+        const noteId = `${TYPE_NAME}:${id}`;
+        const note = cache.readFragment<Note, string>({
+          fragment: NOTE_FRAGMENT,
+          id: noteId,
+        });
+        const updateNote = {
+          ...note,
+          title,
+          content,
+        };
+        cache.writeFragment({
+          id: noteId,
+          fragment: NOTE_FRAGMENT,
+          data: updateNote,
+        });
+        saveNoteAtLocalStorage(cache);
+        return updateNote;
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        return null;
+      }
     },
   },
 };
---
+++ src/types/types.d.ts
@@ -0,0 +1,5 @@
+import { Note } from './graph';
+
+export interface Notes {
+  notes: Note[];
+}
