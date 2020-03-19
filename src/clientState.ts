8e477f284c75c244cfe934fe08b60c1f3166abcd
LeeSuHo <angelxtry@gmail.com>
2020-03-19 22:11:39 +0900

    Data Typename을 상수로 설정 & 불필요한 코드 삭제

--- src/clientState.ts
+++ src/clientState.ts
@@ -4,15 +4,11 @@ import { NOTE_FRAGMENT } from './fragments';
 import { GET_NOTES } from './queries';
 import { saveNoteAtLocalStorage, loadNoteFromLocalStorage } from './offline';
 
-export const defaults = {
-  notes: [
-    ...loadNoteFromLocalStorage()
-  ],
-};
+const TYPE_NAME = 'Note' as const;
 
-// export const defaults = {
-//   notes: loadNoteFromLocalStorage(),
-// };
+export const defaults = {
+  notes: [...loadNoteFromLocalStorage()],
+};
 
 export const typeDefs = gql`
   type Note {
@@ -34,33 +30,27 @@ export const resolvers = {
   Query: {
     note: (
       _: undefined,
-      { id }: { id: string},
+      { id }: { id: string },
       { cache }: { cache: InMemoryCache },
     ) => {
-      const noteId = `Note:${id}`;
+      const noteId = `${TYPE_NAME}:${id}`;
       const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id: noteId });
       return note;
     },
   },
   Mutation: {
     createNote: (_: undefined, args: any, { cache }: any) => {
-      console.log('createNote');
-      console.log(args);
       const data = cache.readQuery({ query: GET_NOTES }) as any;
-      console.log('createNote: ', data);
       const notes = [];
       if (data) {
         notes.push(...data.notes);
       }
-      console.log('createNote - readQuery: ', notes);
-      // console.log(exNotes);
       const newNote = {
-        __typename: 'Note',
+        __typename: TYPE_NAME,
         title: args.title,
         content: args.content,
         id: notes.length + 1,
       };
-      console.log(newNote);
       cache.writeData({
         data: {
           notes: [newNote, ...notes],
@@ -74,7 +64,7 @@ export const resolvers = {
       { id, title, content }: { id: number; title: string; content: string },
       { cache }: { cache: InMemoryCache },
     ) => {
-      const noteId = `NOTE:${id}`;
+      const noteId = `${TYPE_NAME}:${id}`;
       const note = cache.readFragment({
         fragment: NOTE_FRAGMENT,
         id: noteId,
