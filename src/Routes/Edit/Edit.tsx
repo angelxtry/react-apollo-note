b45a789be8df6d5dc7a8caa04dd357d4b8ca6d08
LeeSuHo <angelxtry@gmail.com>
2020-03-19 16:17:45 +0900

    Add Edit component

--- src/Routes/Edit/Edit.tsx
+++ src/Routes/Edit/Edit.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useParams, useHistory, RouteComponentProps } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import Editor from '../../Components/Editor';
+import { GET_NOTE, EDIT_NOTE } from '../../queries';
+import { QueryNoteArgs, MutationEditNoteArgs } from '../../types/graph';
+import { NoteRespose } from '../../types/types';
 
-interface Props {}
+type Props = RouteComponentProps;
+
+type Params = {
+  id: string;
+};
 
 const Edit: React.FC<Props> = () => {
-  return <div>Hello</div>;
+  const params = useParams<Params>();
+  const { data } = useQuery<NoteRespose, QueryNoteArgs>(GET_NOTE, {
+    variables: { id: params.id },
+  });
+
+  const history = useHistory();
+  const [editNoteMutation] = useMutation<NoteRespose, MutationEditNoteArgs>(
+    EDIT_NOTE,
+  );
+  const mutationFn = async (
+    title: string,
+    content: string,
+    id: number | null,
+  ) => {
+    if (title !== '' && content !== '' && id) {
+      await editNoteMutation({ variables: { id, title, content } });
+      history.push('/');
+    }
+  };
+
+  return data?.note ? (
+    <Editor
+      exId={data.note.id}
+      exTitle={data.note.title}
+      exContent={data.note.content}
+      onSave={mutationFn}
+    />
+  ) : (
+    <div>Note not found</div>
+  );
 };
 
 export default Edit;
