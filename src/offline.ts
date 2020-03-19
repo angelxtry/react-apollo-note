b741de65914bc1ad255f29702e989ea2681c2826
LeeSuHo <angelxtry@gmail.com>
2020-03-19 22:41:42 +0900

    Type 추가 & 불필요한 코드 삭제

--- src/offline.ts
+++ src/offline.ts
@@ -1,23 +1,25 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { GET_NOTES } from './queries';
+import { Note } from './types/graph';
+
+const STORAGE_KEY_NAME = 'Notes' as const;
 
 export const saveNoteAtLocalStorage = (cache: InMemoryCache) => {
   const data = cache.readQuery({ query: GET_NOTES });
   const jsonNotes = JSON.stringify(data);
   try {
-    localStorage.setItem('Notes', jsonNotes);
+    localStorage.setItem(STORAGE_KEY_NAME, jsonNotes);
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error);
   }
 };
 
-export const loadNoteFromLocalStorage = (): any[] => {
-  const notes = localStorage.getItem('Notes');
-  console.log(notes);
+export const loadNoteFromLocalStorage = (): Note[] => {
+  const notes = localStorage.getItem(STORAGE_KEY_NAME);
   if (notes) {
     try {
-      const parsedNotes = JSON.parse(notes);
-      console.log(parsedNotes.notes);
+      const parsedNotes = JSON.parse(notes) as { notes: Note[] };
       return parsedNotes.notes;
     } catch (error) {
       return [];
