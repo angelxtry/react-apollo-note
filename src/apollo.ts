45a908bb466479338b38e3c8569ee032b80d68ab
LeeSuHo <angelxtry@gmail.com>
2020-03-19 23:13:47 +0900

    불필요한 코드 삭제

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,24 +1,9 @@
 import { ApolloClient } from 'apollo-client';
-import {
-  InMemoryCache,
-  // IntrospectionFragmentMatcher,
-} from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { withClientState } from 'apollo-link-state';
 import { ApolloLink } from 'apollo-link';
-
-// import introspectionQueryResultData from './introspection.json';
-
 import { defaults, typeDefs, resolvers } from './clientState';
 
-// let fragmentMatcher;
-
-// if (introspectionQueryResultData) {
-//   fragmentMatcher = new IntrospectionFragmentMatcher({
-//     introspectionQueryResultData,
-//   });
-// }
-
-// const cache = new InMemoryCache({ fragmentMatcher });
 const cache = new InMemoryCache({
   addTypename: true,
 });
@@ -27,7 +12,6 @@ const stateLink = withClientState({
   defaults,
   typeDefs,
   resolvers,
-  // fragmentMatcher,
 });
 
 const client = new ApolloClient({
